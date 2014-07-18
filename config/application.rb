57982e1390a99ea43ae17b36cb4c9359a7d27c27
cabox <cabox@box-codeanywhere.com>
2014-07-18 08:56:04 -0400

    Updated asset precompile

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module MCToolkit
   class Application < Rails::Application
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
-    config.assets.precompile += %w(.svg .eot .woff .ttf)
+    config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.jpg *.png)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
