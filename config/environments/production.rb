d852bad6c0a026d5af93697dffb64ffc472b03ee
cabox <cabox@box-codeanywhere.com>
2014-07-18 08:49:11 -0400

    added Heroku precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ MCToolkit::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.cache_classes = true
+  config.serve_static_assets = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
