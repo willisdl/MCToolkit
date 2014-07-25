f85007bc25dbb416ef5afba01de0a425b0b2dcd2
cabox <cabox@box-codeanywhere.com>
2014-07-25 12:08:44 -0400

    Creating default current exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   $rolelist = Role.all
   $current_ex = CurrentExercise.first
-  if CurrentExercise.first != nil && CurrentExercise.first != 'Blank'
+  if CurrentExercise.first != nil && CurrentExercise.first.name != 'Blank'
     $unitlist = getunits
   end
 
