c7a578bb453593e7852b03019eef0499ea1f8207
David Willis <david.l.willis@me.com>
2015-05-10 16:09:32 -0400

    Generated scaffold for observations

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,15 +1,4 @@
 module HomesHelper
 
-  # Proxy to devise map name
-  def resource_name
-    devise_mapping.name
-  end
 
-  def resource
-    instance_variable_get(:"@#{resource_name}")
-  end
-
-  def devise_mapping
-    @devise_mapping ||= request.env["devise.mapping"]
-  end
 end
