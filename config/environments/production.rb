4434d9d83e6ea3f078b75a84ea32d762581bf040
David Willis <david.l.willis@me.com>
2015-05-12 08:35:02 -0400

    Generated scaffold for observations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ MCToolkit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  Rails.application.routes.default_url_options[:host] = 'mctoolkit-navbar.herokuapp.com'
+  # Rails.application.routes.default_url_options[:host] = 'mctoolkit-navbar.herokuapp.com'
 
   config.action_mailer.default_url_options = { :host => 'mctoolkit-navbar.herokuapp.com' }
 
